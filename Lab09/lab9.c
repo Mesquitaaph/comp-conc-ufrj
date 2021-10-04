f02a2c512783347f433799582f08847a72b39936
mesquitaaph <raphafm.rf@gmail.com>
2021-10-04 12:20:25 -0300

    testes com números de consumidores e produtores variados

--- Lab09/lab9.c
+++ Lab09/lab9.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-// #include <semaphore.h>
+#include <semaphore.h>
 
-#define NTHREADS 4
+#define Prod 5
+#define Cons 1
 #define N 4
 
 sem_t mutexProd, mutexCons; // Exclusão mútua
 sem_t bufferCheio, slotVazio; // Condição
 int Buffer[N];
 
+void sleep() {
+  long int ticks = 1000000000;
+  for(long int i = 0; i < ticks; i++) {}
+}
+
 void Retira (int* elementos) {
-  int out=0;
+  int out = 0;
   // Espera encher o buffer
   sem_wait(&bufferCheio);
   //exclusao mutua entre consumidores
   sem_wait(&mutexCons);
+  // printf("Entrou no Retira() -- out = %d\n", out);
   for(int i = 0; i < N; i++) {
     elementos[i] = Buffer[out];
     out = (out + 1) % N;
@@ -33,11 +40,12 @@ void Insere (int item) {
   sem_wait(&slotVazio);
   //exclusao mutua entre produtores
   sem_wait(&mutexProd);
+  printf("Entrou no Insere() -- in = %d\n", in);
   Buffer[in] = item;
-  in = (in + 1) % N;
+  in++;
   sem_post(&mutexProd);
   //sinaliza um slot cheio
-  if(in == N - 1) {
+  if(in == N) {
     in = 0;
     sem_post(&bufferCheio);
   }
@@ -46,8 +54,9 @@ void Insere (int item) {
 void *produtor(void * arg) {
   int elemento = 0;
   while(1) {
-    Insere(elemento);
+    Insere(elemento+*(int*)arg);
     elemento++;
+    sleep();
   }
   pthread_exit(NULL);
 }
@@ -57,12 +66,16 @@ void *consumidor(void * arg) {
   elementos = (int*) malloc(sizeof(int) * N);
   while(1) {
     Retira(elementos);
+    printf("Consumidor retirando id = %d\n", *(int*)arg);
     for(int i = 0; i < N; i++) printf("%d%c\n", elementos[i], i == N - 1 ? '\n' : ' ');
+    sleep();
   }
+  free(elementos);
   pthread_exit(NULL);
 }
 
 int main() {
+  int NTHREADS = Prod + Cons;
   pthread_t tid[NTHREADS];
   int *id[NTHREADS], i;
 
@@ -77,14 +90,16 @@ int main() {
   sem_init(&mutexCons, 0, 1); // Semáforo binário, para exclusão mútua
   sem_init(&mutexProd, 0, 1); // Semáforo binário, para exclusão mútua
   sem_init(&bufferCheio, 0, 0);
-  sem_init(&slotVazio, 0, NTHREADS);
+  sem_init(&slotVazio, 0, N);
 
   //cria as tres threads
-  if (pthread_create(&tid[1], NULL, welcome, (void *)id[1])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
-  if (pthread_create(&tid[0], NULL, staying, (void *)id[0])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
-  if (pthread_create(&tid[3], NULL, staying, (void *)id[3])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
-  if (pthread_create(&tid[2], NULL, goodbye, (void *)id[2])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  for(i = 0; i < Prod; i++) {
+    if (pthread_create(&tid[i], NULL, produtor, (void *)id[i])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  }
 
+  for(; i < Prod + Cons; i++) {
+    if (pthread_create(&tid[i], NULL, consumidor, (void *)id[i])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  }
   //--espera todas as threads terminarem
   for (i=0; i<NTHREADS; i++) {
     if (pthread_join(tid[i], NULL)) {
